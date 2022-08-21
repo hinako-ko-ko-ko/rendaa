fdd529e62661780b5a23c4cb36c308281b869b1d
Hinako Nakai <hinako.0113n@gmail.com>
2022-08-21 19:20:57 +0900

    renda

--- renda/ViewController.swift
+++ renda/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var plusbutton: UIButton!
+    var tap  = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        plusbutton.layer.cornerRadius = 100
     }
-
+    @IBAction func tapplusbutton(){
+        tap = tap+1
+        label.text = String(tap)
+        
+        
+    }
 
 }
 
